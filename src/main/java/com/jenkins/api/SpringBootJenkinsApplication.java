d3807ca0c2fa6646c5f4596606cde5d408a3b5de
ahmadtausif38 <ahmadtausif38@gmail.com>
2023-11-27 12:13:38 +0530

    Second Commit

--- src/main/java/com/jenkins/api/SpringBootJenkinsApplication.java
+++ src/main/java/com/jenkins/api/SpringBootJenkinsApplication.java
@@ -5,16 +5,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SpringBootJenkinsApplication {
 	
-	Logger logger=LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+	public static Logger logger=LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
 	
+	
+	@PostConstruct
 	public void init() {
 		logger.info("Application Started....");
 	}
 
 	public static void main(String[] args) {
+		logger.info("Application executed....");
+		logger.info("Application is integrated with Jenkins..");
 		SpringApplication.run(SpringBootJenkinsApplication.class, args);
 	}
 
